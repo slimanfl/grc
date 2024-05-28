c1c9ffe36557517f017d4fc2aace46e2e4776435
slimanfl <slimanite1996@gmail.com>
2024-05-27 18:20:04 -0700

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,53 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      spacing: {
+        //spacing
+      },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily: {
+        roboto: "Roboto",
+      },
+      fontSize: {
+        mm: ["14px", "20px"],
+      },
+      letterSpacing: {
+        bb: "2rem",
+      },
+      // width: {
+      //   //width
+      // },
+      // height: {
+      //   //hright
+      // },
+    },
+  },
+  plugins: [],
+};
+export default config;
